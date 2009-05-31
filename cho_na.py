e34139d5dc69ce056df20c08b5fb80f9ce81617f
Jonathan G. Underwood <j.underwood@ucl.ac.uk>
2009-06-01 00:06:58 +0100

    Use numpy.fromfunction to compose P matrix rather than step thru' by hand

--- cho_na.py
+++ cho_na.py
@@ -7,33 +7,30 @@
 import numpy
 import math
 
+def __P(i, j):
+    """
+    Calculates the Cho and Na P matrix elements assuming d = 1.
+    """
+    # Note that here we get away with casting i and j to float, which is
+    # surprising. Note that in the future, if this is required, it is achieved
+    # by jj = j + 1.0, i.astype(float) etc.
+    jj = j + 1.0
+    theta = numpy.arccos(i / jj)
+    return numpy.where(j >= i, 
+                       0.5 * (jj * jj * theta - i * i * numpy.tan(theta)), 
+                       0.0)
+
 def area_matrix(dim):
     """
     Calculate and return a numpy array containing the area matrix as defined
     by Cho and Na. The returned square array is of size dim. The array is
     normalized to unit width.
     """
-    # Avoid dictionary lookup for functions in loops
-    acos = math.acos
-    tan = math.tan
-    
-    # Note that the Cho Na i, j indices in their paper start from 1 rather
-    # than 0. Here we index from zero, so the formulas are adjusted
-    # accordingly.
+    P = numpy.fromfunction(__P, (dim, dim))
 
-    P = numpy.zeros((dim, dim))
-    
-    # Here we set d = 1 / dim^2 to normalize to a width of 1. This isn't
-    # really necessary - we could dispense with this and the factor of 0.5 in
-    # the loop
-    dd = 1.0 / (dim * dim)
-    for i in xrange(dim):
-        ii = float(i)
-        for j in xrange(i, dim):
-            jj = float(j + 1)
-            theta = acos(ii / jj)
-            P[i, j] = 0.5 * dd * (jj * jj * theta - 
-                                  ii * ii * tan(theta))
+    # Here we set d = 1 / dim^2 to normalize to a width of 1.  This isn't
+    # really necessary. Likewise we could drop the 0.5 in __P above.
+    P = numpy.multiply(P, 1.0 / (dim * dim))
 
     S = P.copy()
     S[0:dim - 1, :] -= P[1:dim, :]
