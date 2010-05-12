9156800357e66fcbdcd2b0dcb807c13fd76d2862
Jonathan G. Underwood <j.underwood@ucl.ac.uk>
2010-05-12 01:10:34 +0100

    Modify test.py to use vmi.py classes

--- test.py
+++ test.py
@@ -1,8 +1,7 @@
 import sys
-import vmipolar
-import vmicart
 import numpy
 import pylab
+import vmi
 
 file = sys.argv[1]
 
@@ -12,18 +11,20 @@ except IOError:
     print "Could not read file", file
     sys.exit(74)
 
-cimage = vmicart.VMICartesianImage()
+cimage = vmi.VMICartesianImage()
 cimage.from_numpy_array(image)
-#print 'centre of gravity:', cimage.centre_of_gravity()
+print 'centre of gravity:', cimage.centre_of_gravity()
+print 'centre of grid:', cimage.centre_of_grid()
+cimage.set_centre(cimage.centre_of_grid())
 
 pylab.figure()
 pylab.imshow(cimage.image, origin='lower')
 pylab.show()
 
-pimage = vmipolar.VMIPolarImage()
+pimage = vmi.VMIPolarImage()
 pimage.from_VMICartesianImage(cimage)
 
-cimage2 = vmicart.VMICartesianImage()
+cimage2 = vmi.VMICartesianImage()
 cimage2.from_VMIPolarImage(pimage)
 
 pylab.figure()
