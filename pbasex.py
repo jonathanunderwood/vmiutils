fae220bb14ee5a4296790ad1de5e50919b7de3e2
Jonathan G. Underwood <j.underwood@ucl.ac.uk>
2010-05-29 20:37:10 +0100

    Updates of pbasex.py prior to pushing

--- pbasex.py
+++ pbasex.py
@@ -1,21 +1,37 @@
 import numpy
 import scipy.integrate
 
-def calc_basis(k, l, ir, itheta):
-    """Calculates the value of a basis function specified by k and l indices
-    corresponding to the point (r, theta) in the velocity map image by
+def calc_basis(k, l, i, j):
+    """Calculates the value of a basis function, specified by k and l indices,
+    corresponding to the point (R, Theta) in the velocity map polar image by
     the Abel integral."""
-    
-    scipy.integrate.quad(__basis_integrand, 0, 
     pass
 
 
-def calc_matrix(kmax, lmax, rmax, rbins, thetabins, oddl=True):
+def __integrand(r, R, Theta, sigma, rk):
     
-    kdim = kmax + 1
-    ldim = lmax +1
 
-    mtx = numpy.fromfunction(calc_basis, (kdim, ldim, rbins, thetabins))
+
+def calc_matrix(kmax, lmax, rmax, rbins, thetabins, oddl=True):
+
+    kdim = kmax + 1
+    ldim = lmax + 1
+
+    dR = rmax / Rbins
+    dTheta = 2.0 * numpy.pi / Thetabins
+
+    sigma = 
+    mtx = numpy.empty((kdim, ldim, rbins, thetabins))
+
+    for k in xrange(kdim):
+        for l in xrange(ldim):
+            for i in xrange(Rbins):
+                for j in xrange(Thetabins):
+                    R = i * dR
+                    Theta = j * dTheta
+                    scipy.integrate.quad(__basis_integrand, R, scipy.infty, 
+                                         args=(R, Theta, sigma, rk)) 
+
     mtx = mtx.reshape((kdim * ldim, rbins * thetabins))
         
     return mtx
