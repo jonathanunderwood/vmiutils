ff5ef1d632c3075411bb56f5c01d7632aaa56583
Jonathan G. Underwood <j.underwood@ucl.ac.uk>
2010-06-04 16:20:18 +0100

    Add basisfn.c

---
+++ basisfn.c
@@ -0,0 +1,31 @@
+#include <Python.h>
+
+static double integrand(r)
+{
+
+}
+
+static PyObject *
+basisfn(PyObject *self, PyObject *args)
+{
+  int k, l; /* Could use unsigned int here. */
+  double R, Theta;
+
+  if (!PyArg_ParseTuple(args, "i i d d", &k, &l, &R, &Theta)
+    return NULL;
+
+
+
+}
+static PyMethodDef BasisFnMethods[] = {
+    {"basisfn",  basisfn, METH_VARARGS,
+     "Calculate the value of a basis function."},
+    {NULL, NULL, 0, NULL}        /* Sentinel */
+};
+
+PyMODINIT_FUNC
+initbasisfn(void)
+{
+    (void) Py_InitModule("basisfn", BasisFnMethods);
+}
+
