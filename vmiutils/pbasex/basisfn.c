7895d4503170215fb7d91b1454cbcfa1708d485f
Jonathan G. Underwood <j.underwood@ucl.ac.uk>
2010-06-08 00:24:31 +0100

    Edits to pbasex/basisfn.c to allow module to compile

--- vmiutils/pbasex/basisfn.c
+++ vmiutils/pbasex/basisfn.c
@@ -2,7 +2,7 @@
 
 static double integrand(r)
 {
-
+  return 0.0;
 }
 
 static PyObject *
@@ -11,10 +11,8 @@ basisfn(PyObject *self, PyObject *args)
   int k, l; /* Could use unsigned int here. */
   double R, Theta;
 
-  if (!PyArg_ParseTuple(args, "i i d d", &k, &l, &R, &Theta)
+  if (!PyArg_ParseTuple(args, "i i d d", &k, &l, &R, &Theta))
     return NULL;
-
-
 
 }
 static PyMethodDef BasisFnMethods[] = {
