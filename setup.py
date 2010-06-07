cad751789ef135d37c27ea09c9dd3141641ef5b2
Jonathan G. Underwood <j.underwood@ucl.ac.uk>
2010-06-07 20:55:19 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from distutils.core import setup, Extension
 
-setup(name='VMIinvert',
+setup(name='VMIutils',
       version='0.1'
       description='Programs for inverting VMI image',
       author='Jonathan G. Underwood',
       author_email='j.underwood@ucl.ac.uk',
-      packages=['pbasex', 'ChoNa'],
-      ext_modules=[Extension('pbasex.basisfn', 
-                             ['pbasex/basisfn.c'],
+      packages=['vmiutils', 'vmiutils.pbasex', 'vmiutils.ChoNa'],
+      ext_modules=[Extension('vmiutils.pbasex.basisfn', 
+                             ['vmiutils/pbasex/basisfn.c'],
                              libraries=['gsl', 'gslcblas', 'm'])],
       )
