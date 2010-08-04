cf39b27cdf7323c21c9770d2cd6d9497668582bb
Jonathan G. Underwood <j.underwood@ucl.ac.uk>
2010-08-05 00:59:41 +0100

    Rename some attributes in VMIPolarImage to be more consistent with other code

--- vmiutils/vmi.py
+++ vmiutils/vmi.py
@@ -39,7 +39,7 @@ class VMICartesianImage():
         """
         self.x, self.y, self.image = pimage.cartesian_rep()
 
-    def polar_rep(self, radial_bins=None, angular_bins=None, max_radius=None):
+    def polar_rep(self, Rbins=None, Thetabins=None, Rmax=None):
         """ Returns a tuple (r, theta, pimage) containing the coordinates and
         polar representation of the image.
         """
@@ -49,14 +49,14 @@ class VMICartesianImage():
         if self.centre == None:
             raise ValueError ## FIXME
 
-        if radial_bins == None:
-            radial_bins = min(self.image.shape[0], self.image.shape[1]) 
+        if Rbins == None:
+            Rbins = min(self.image.shape[0], self.image.shape[1]) 
 
-        if angular_bins == None:
-            angular_bins = min(self.image.shape[0], self.image.shape[1]) 
+        if Thetabins == None:
+            Thetabins = min(self.image.shape[0], self.image.shape[1]) 
 
         return polcart.cart2pol(self.image, self.x, self.y, self.centre, 
-                                radial_bins, angular_bins, max_radius)
+                                Rbins, Thetabins, Rmax)
 
     def centre_of_gravity(self):
         """ Returns a tuple representing the coordinates corresponding to the
@@ -79,24 +79,26 @@ class VMIPolarImage():
 
     def __init__(self):
         self.image = None
-        self.r = None
-        self.theta = None
-        self.rfactor = None
+        self.R = None
+        self.Theta = None
+        self.Rfactor = None
 
-    def from_VMICartesianImage(self, cimage, radial_bins=None, 
-                               angular_bins=None, max_radius=None):
+    def from_VMICartesianImage(self, cimage, Rbins=None, 
+                               Thetabins=None, Rmax=None):
         """Calculate a polar represenation of a VMICartesianImage instance.
 
         cimage is a VMICartesianImage instance.
 
-        radial_bins and angular_bins specify the desired number of bins in the
+        Rbins and Thetabins specify the desired number of bins in the
         polar representation. If these are none, the number of bins in the
         cartesian image is used.
         """
-        self.r, self.theta, self.image = \
-            cimage.polar_rep(radial_bins, angular_bins, max_radius)
+        self.R, self.Theta, self.image = \
+            cimage.polar_rep(Rbins, Thetabins, Rmax)
 
-        self.rfactor = self.r[1] - self.r[0]
+        self.Rfactor = self.R[1] - self.R[0]
+        self.Rbins = self.R.shape[0]
+        self.Thetabins = self.Theta.shape[0]
 
     def cartesian_rep(self, xbins=None, ybins=None):
         """ Returns a tuple (x, y, image) containing the coordinates and
@@ -111,4 +113,4 @@ class VMIPolarImage():
         if ybins == None:
             ybins = self.image.shape[0]
 
-        return polcart.pol2cart(self.image, self.r, self.theta, xbins, ybins)
+        return polcart.pol2cart(self.image, self.R, self.Theta, xbins, ybins)
