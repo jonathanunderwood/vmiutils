e0a2a7c20598980b85b7fb6b20ca8b4371e55f1a
Jonathan G. Underwood <j.underwood@ucl.ac.uk>
2010-08-19 23:52:41 +0100

    Add alternative matrix calculation which minimises numpy API calls

--- vmiutils/pbasex/_matrix.c
+++ vmiutils/pbasex/_matrix.c
@@ -324,6 +324,227 @@ matrix(PyObject *self, PyObject *args)
   return NULL;
 }
 
+static PyObject *
+matrix2(PyObject *self, PyObject *args)
+{
+  int lmax, kmax, Rbins, Thetabins;
+  double sigma, epsabs, epsrel; /* Suggest epsabs = 0.0, epsrel = 1.0e-7 */   
+  double rkspacing, dTheta;
+  int wkspsize; /* Suggest: wkspsize = 100000. */
+  int ldim, kdim, midTheta, k;
+  unsigned short int oddl, ThetabinsOdd, linc;
+  npy_intp dims[4];
+  double *matrix;
+  PyObject *matrixnp;
+  gsl_integration_workspace *wksp;
+  gsl_function fn;
+  int_params params;
+  gsl_integration_qaws_table *table;
+  int index = -1;
+
+  if (!PyArg_ParseTuple(args, "iiiidHddi", 
+			&kmax, &lmax, &Rbins, &Thetabins, &sigma, &oddl, &epsabs, &epsrel, &wkspsize))
+    {
+      PyErr_SetString (PyExc_TypeError, "Bad argument to matrix");
+      return NULL;
+    }
+
+  kdim = kmax + 1;
+
+  /* If oddl is 0 (false), we only consider even harmonics, and adjust the
+     indexing accordingly. */
+  if (oddl)
+    {
+      ldim = lmax + 1;
+      linc = 1;
+    }
+  else
+    {
+      if (GSL_IS_ODD(lmax))
+	{
+	  PyErr_SetString (PyExc_TypeError, "oddl is 0 (false), but lmax is odd.");
+	  return NULL;
+	}
+      else /* lmax is even */
+	{
+	  ldim = (lmax / 2) + 1;
+	  linc = 2;
+	}
+    }
+
+  matrix = malloc (Rbins * Thetabins * kdim * ldim * sizeof(double));
+  if (!matrix)
+    return PyErr_NoMemory();
+  
+  /* Turn off gsl error handler - we'll check return codes. */
+  gsl_set_error_handler_off ();
+
+  wksp = gsl_integration_workspace_alloc(wkspsize);
+  if (!wksp)
+    {
+      free (matrix);
+      return PyErr_NoMemory();
+    }
+
+  table = gsl_integration_qaws_table_alloc(-0.5, 0.0, 0.0, 0.0);
+  if (!table)
+    {
+      free (matrix);
+      gsl_integration_workspace_free(wksp);
+      return PyErr_NoMemory();
+    }
+
+  fn.function = &integrand;
+  fn.params = &params;
+
+  rkspacing = ((double) Rbins) / kdim;
+
+  params.two_sigma2 = 2.0 * sigma * sigma;
+
+  /* We create a matrix for Theta = -Pi..Pi inclusive of both endpoints, despite
+     the redundancy of the last (or first) endpoint. We use the symmetry of the
+     Legendre polynomials P_L(cos(theta))=P_L(cos(-theta)) to calculate the
+     points in the range 0..Pi from those in the range -Pi..0. 
+
+     If Thetabins is an odd number, there will be a point at theta = 0, for
+     which we don't need to use the symmetry condition. If Thetabins is an even
+     number, then there won't be a point at Theta = 0. midTheta defines the last
+     point for which we need to do the calculation in either case.
+  */
+  dTheta = 2.0 * M_PI / (Thetabins - 1);
+  midTheta = Thetabins / 2; /* Intentionally round down. */
+  
+  if (GSL_IS_EVEN(Thetabins))
+    {
+      midTheta--;
+      ThetabinsOdd = 0;
+    }
+  else
+    {
+      ThetabinsOdd = 1;
+    }
+
+  for (k=0; k<=kmax; k++)
+    {
+      int l;
+      double upper_bound;
+      
+      params.rk = k * rkspacing;
+      upper_bound = params.rk + __UPPER_BOUND_FACTOR * sigma;
+
+      for (l = 0; l <= lmax; l += linc)
+	{
+	  int R;
+	  params.l = l;
+	  for (R = 0; R < Rbins; R++)
+	    {
+	      int j;
+	      params.R2 = (double) (R * R);
+	      for (j = 0; j <= midTheta; j++)
+		{
+		  int status;
+		  double result, abserr;
+		  double Theta = -M_PI + j * dTheta;
+
+		  params.RcosTheta = R * cos (Theta);
+		  
+		  if (upper_bound > R)
+		    {
+		      status = gsl_integration_qaws (&fn, (double) R, upper_bound, table,
+						     epsabs, epsrel, wkspsize,
+						     wksp, &result, &abserr);
+		    }
+		  else 
+		    {
+		      status = 0;
+		      result = 0.0;
+		      abserr = 0.0;
+		    }
+
+		  index++;
+
+		  switch (status)
+		    {
+		    case GSL_SUCCESS:
+		      matrix[index] = result;
+
+		      /* Symmetry of Legendre polynomials is such that
+			 P_L(cos(Theta))=P_L(cos(-Theta)), so we can exploit
+			 that here unless Theta = 0, in which case it's not
+			 needed. 
+
+			 Note on how we worked out the indexing: for odd l,
+
+			 index = (k * kdim) + (l * ldim) + (R * Rbins) + j
+
+			 for positive negative theta, while for positive theta
+
+			 index' = (k * kdim) + (l * ldim) + (R * Rbins) +
+			           Thetabins - j - 1
+
+			 and so the difference is Thetabins - 2j -1
+		      */
+		      if (!(ThetabinsOdd && j == midTheta))
+			matrix[index + Thetabins - (2 * j) - 1] = result;
+
+		      break;
+
+		    case GSL_EMAXITER:
+		      PyErr_SetString (MaxIterError, 
+				       "Maximum number of integration subdivisions exceeded");
+		      goto fail;
+		      
+		    case GSL_EROUND:
+		      printf("k:%d l:%d R:%d Theta: %f\n", k, l, R, Theta);
+		      PyErr_SetString (RoundError, "Failed to integrate: round-off error");
+		      goto fail;
+		      
+		    case GSL_ESING:
+		      printf("k:%d l:%d R:%d Theta: %f\n", k, l, R, Theta);
+		      PyErr_SetString (SingularError, "Failed to integrate: singularity found");
+		      goto fail;
+		      
+		    case GSL_EDIVERGE:
+		      printf("k:%d l:%d R:%d Theta: %f\n", k, l, R, Theta);
+		      PyErr_SetString (DivergeError, "Failed to integrate: divergent or slowly convergent");
+		      goto fail;
+		      
+		    default:
+		      printf("k:%d l:%d R:%d Theta: %f\n", k, l, R, Theta);
+		      PyErr_SetString (PyExc_RuntimeError, "Failed to integrate: Unknown error");
+		      goto fail;
+		    }	
+		}
+
+	    }
+	}
+    }
+
+  gsl_integration_workspace_free(wksp);
+  gsl_integration_qaws_table_free(table);
+
+
+  /* Create numpy array to hold the matrix. */
+  dims[0] = (npy_intp) kdim;
+  dims[1] = (npy_intp) ldim;
+  dims[2] = (npy_intp) Rbins;
+  dims[3] = (npy_intp) Thetabins;
+
+  matrixnp = PyArray_SimpleNewFromData (4, dims, NPY_DOUBLE, matrix);
+
+  // TODO: better error checking here.
+  if (!matrixnp)
+    return PyErr_NoMemory();
+
+  return matrixnp;
+
+ fail:  
+  gsl_integration_workspace_free(wksp);
+  gsl_integration_qaws_table_free(table);
+  Py_DECREF(matrix);
+
+  return NULL;
+}
 
 /* Module function table. Each entry specifies the name of the function exported
    by the module and the corresponding C function. */
@@ -331,6 +552,8 @@ static PyMethodDef MatrixMethods[] = {
     {"basisfn",  basisfn, METH_VARARGS,
      "Returns the value of a basis function."},
     {"matrix",  matrix, METH_VARARGS,
+     "Returns an inversion matrix of basis functions."},
+    {"matrix2",  matrix2, METH_VARARGS,
      "Returns an inversion matrix of basis functions."},
     {NULL, NULL, 0, NULL}        /* Sentinel */
 };
