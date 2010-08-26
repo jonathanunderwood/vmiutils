More progress towards getting correct bet coefficient calculation

tests/test_reshape4d.py
@@ -1,16 +1,26 @@
 import numpy as np
 
-N=2
-
-a=np.empty((N, N, N, N), dtype=tuple)
+N=3
+M = 2
+O = 3
+P = 2
+a=np.empty((N, M, O, P), dtype=tuple)
 
 for i in xrange(N):
-    for j in xrange(N):
-        for k in xrange(N):
-            for l in xrange(N):
+    for j in xrange(M):
+        for k in xrange(O):
+            for l in xrange(P):
                 a[i][j][k][l]=(i, j, k, l)
 
 print a
 print 'Reshaped:'
-print a.reshape(N*N, N*N)
+b=a.reshape(N*M, O*P)
+print b
 
+n =1
+m=0
+o=1
+p=1
+
+print a[n,m,o,p]
+print b[n*M+m, o*P+p]