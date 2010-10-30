b5a3a2d81ed376cb4960cd6df41c8c8ae05e754a
Jonathan G. Underwood <j.underwood@ucl.ac.uk>
2010-10-30 09:14:29 +0100

    Properly use ndarray and python API in _fit.c

--- vmiutils/pbasex/_fit.c
+++ vmiutils/pbasex/_fit.c
@@ -4,6 +4,62 @@
 #include <math.h>
 #include <gsl/gsl_sf_legendre.h>
 
+static inline int
+arr1D_get(PyObject *arr, int idx, double *val)
+{
+  PyObject *pval = PyArray_GETITEM(arr, PyArray_GETPTR1(arr, idx));
+  if (pval)
+    {
+      *val = PyFloat_AsDouble(pval);
+      Py_DECREF(pval);
+      return 0;
+    }
+  else
+    return -1;
+}
+
+static inline int
+arr1D_set(PyObject *arr, int idx, double val)
+{
+  PyObject *pval = PyFloat_FromDouble (val);
+  if (pval)
+    {
+      int ret = PyArray_SETITEM(arr, PyArray_GETPTR1(arr, idx), pval);
+      Py_DECREF(pval);
+      return ret;
+    }
+  else
+    return -1;
+}
+
+static inline int
+arr2D_get(PyObject *arr, int idx1, int idx2, double *val)
+{
+  PyObject *pval = PyArray_GETITEM(arr, PyArray_GETPTR2(arr, idx1, idx2));
+  if (pval)
+    {
+      *val = PyFloat_AsDouble(pval);
+      Py_DECREF(pval);
+      return 0;
+    }
+  else
+    return -1;
+}
+
+static inline int
+arr2D_set(PyObject *arr, int idx1, int idx2, double val)
+{
+  PyObject *pval = PyFloat_FromDouble (val);
+  if (pval)
+    {
+      int ret = PyArray_SETITEM(arr, PyArray_GETPTR2(arr, idx1, idx2), pval);
+      Py_DECREF(pval);
+      return ret;
+    }
+  else
+    return -1;
+}
+
 static PyObject *
 polar_distribution(PyObject *self, PyObject *args)
 {
@@ -47,9 +103,7 @@ polar_distribution(PyObject *self, PyObject *args)
 	{
 	  double theta = j * thetastep;
 	  int k;
-	  double *distp = (double *) PyArray_GETPTR2(dist, i, j);
-	  
-	  *distp = 0.0;
+	  double val = 0;
 
 	  for (k = 0; k <= kmax; k++)
 	    {
@@ -61,17 +115,26 @@ polar_distribution(PyObject *self, PyObject *args)
 	      for (l = 0; l <= lmax; l++)
 		{
 		  double ang = gsl_sf_legendre_Pl(l, cos(theta));
-		  double *cvalp = (double *) PyArray_GETPTR2(coef, k, l);
+		  double c;
+		  if (arr2D_get(coef, k, l, &c))
+		    goto fail;
 
-		  *distp += *cvalp * rad * ang;
+		  val += c * rad * ang;
 		}
 	    }
+	  if (arr2D_set(dist, i, j, val))
+	    goto fail;
 	}
     }
 
   Py_DECREF(coef);
 
   return dist;
+
+ fail:
+  Py_DECREF(coef);
+  Py_DECREF(dist);
+  return NULL;
 }
 
 static PyObject *
@@ -123,10 +186,13 @@ beta_coeffs(PyObject *self, PyObject *args)
 
 	  for (l = 0; l <= lmax; l++)
 	    {
-	      double *cvalp = (double *) PyArray_GETPTR2(coef, k, l);
-	      double *betap = (double *) PyArray_GETPTR2(beta, l, i);
-
-	      *betap += *cvalp * rad;
+	      double b, c;
+	      if (arr2D_get(coef, k, l, &c))
+		goto fail;
+	      if (arr2D_get(beta, l, i, &b))
+		goto fail;
+	      if (arr2D_set(beta, l, i, b + c * rad))
+		goto fail;
 	    }
 	}
     }
@@ -136,21 +202,30 @@ beta_coeffs(PyObject *self, PyObject *args)
   /* Normalize to beta_0 = 1 at each r. */
   for (i = 0; i < rbins; i++)
     {
-      double *normp = (double *) PyArray_GETPTR2(beta, 0, i);
+      double norm;
       int l;
-      
+
+      if (arr2D_get(beta, 0, i, &norm))
+	goto fail;
+
       for (l = 0; l <= lmax; l++)
 	{
-	  double *betap = (double *) PyArray_GETPTR2(beta, l, i);
-      
-	  /* if (norm > 0.0) */
-	  *betap /= *normp;
-	  /* else */
-	  /*   beta[index] = 0.0; */
+	  double b;
+
+	  if (arr2D_get(beta, l, i, &b))
+	    goto fail;
+
+	  if (arr2D_set(beta, l, i, b / norm))
+	    goto fail;
 	}
     }
 
   return beta;
+
+ fail:
+  Py_DECREF(beta);
+  Py_DECREF(coef);
+  return NULL;
 }
 
 // TODO: speed up calculation by making use of mirror symmetry in y
@@ -196,9 +271,7 @@ cartesian_distribution(PyObject *self, PyObject *args)
 	{
 	  double y = -rmax + j * step;
 	  double r = sqrt (x * x + y * y);
-	  double *distp = (double *) PyArray_GETPTR2(dist, i, j);
-
-	  *distp = 0.0;
+	  double val = 0.0;
 
 	  if (r < rmax)
 	    {
@@ -215,18 +288,28 @@ cartesian_distribution(PyObject *self, PyObject *args)
 		  for (l = 0; l <= lmax; l++)
 		    {
 		      double ang = gsl_sf_legendre_Pl(l, costheta);
-		      double *cvalp = (double *) PyArray_GETPTR2(coef, k, l);
+		      double c;
+		      
+		      if (arr2D_get(coef, k, l, &c))
+			goto fail;
 
-		      *distp += *cvalp * rad * ang;
+		      val += c * rad * ang;
 		    }
 		}
 	    }
+	  if (arr2D_set(dist, i, j, val))
+	    goto fail;
 	}
     }
 
   Py_DECREF(coef);
 
   return dist;
+  
+ fail:
+  Py_DECREF(coef);
+  Py_DECREF(dist);
+  return NULL;
 }
 
 static PyObject *
@@ -265,7 +348,6 @@ radial_spectrum(PyObject *self, PyObject *args)
     {
       double r = i * rstep;
       double val = 0.0;
-      double *specp = (double *) PyArray_GETPTR1(spec, i);
       int k;
 
       for (k = 0; k <= kmax; k++)
@@ -273,12 +355,16 @@ radial_spectrum(PyObject *self, PyObject *args)
 	  double rk = k * rkstep;
 	  double a = r - rk;
 	  double rad = exp(-(a * a) / s);
-	  double *cvalp = (double *) PyArray_GETPTR2(coef, k, 0);
+	  double c;
 
-	  val += (*cvalp) * rad * r * r;
+	  if (arr2D_get(coef, k, 0, &c))
+	    goto fail;
+
+	  val += c * rad * r * r;
 	}
 
-      *specp = val;
+      if (arr1D_set(spec, i, val))
+	goto fail;
 
       if (val > max)
 	max = val;
@@ -289,12 +375,21 @@ radial_spectrum(PyObject *self, PyObject *args)
   /* Normalize to maximum value of 1. */
   for (i = 0; i < rbins; i++)
     {
-      double *specp = (double *) PyArray_GETPTR1(spec, i);
+      double val;
 
-      *specp /= max;
+      if (arr1D_get(spec, i, &val))
+	goto fail;
+
+      if (arr1D_set(spec, i, val / max))
+	goto fail;
     }
 
   return spec;
+
+ fail:
+  Py_DECREF(coef);
+  Py_DECREF(spec);
+  return NULL;
 }
 
 /* Module function table. Each entry specifies the name of the function exported
