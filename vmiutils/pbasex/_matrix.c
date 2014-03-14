c49b0d24dbc2e2b9ab612fdb21bf2c8f4776d4a1
Jonathan Underwood <jonathan.underwood@gmail.com>
2014-03-14 11:42:46 +0000

    Add a new function to return a single basis function to _matrix.c

--- vmiutils/pbasex/_matrix.c
+++ vmiutils/pbasex/_matrix.c
@@ -49,25 +49,25 @@ static double integrand(double r, void *params)
     return 0.0;
 }
 
-static PyObject *
-basisfn(PyObject *self, PyObject *args)
-{
-  int l;
-  double r, rk, sigma, theta, rad, ang, a, s;
+/* static PyObject * */
+/* basisfn(PyObject *self, PyObject *args) */
+/* { */
+/*   int l; */
+/*   double r, rk, sigma, theta, rad, ang, a, s; */
 
-  if (!PyArg_ParseTuple(args, "dddid", &r, &rk, &sigma, &l, &theta))
-    {
-      PyErr_SetString (PyExc_TypeError, "Bad argument to basisfn_full");
-      return NULL;
-    }
+/*   if (!PyArg_ParseTuple(args, "dddid", &r, &rk, &sigma, &l, &theta)) */
+/*     { */
+/*       PyErr_SetString (PyExc_TypeError, "Bad argument to basisfn_full"); */
+/*       return NULL; */
+/*     } */
 
-  a = r - rk;
-  s = 2.0 * sigma * sigma;
-  rad = exp(-(a * a) / s);
-  ang = gsl_sf_legendre_Pl(l, cos(theta));
+/*   a = r - rk; */
+/*   s = 2.0 * sigma * sigma; */
+/*   rad = exp(-(a * a) / s); */
+/*   ang = gsl_sf_legendre_Pl(l, cos(theta)); */
 
-  return Py_BuildValue("d", rad * ang);
-}
+/*   return Py_BuildValue("d", rad * ang); */
+/* } */
 
 static PyObject *
 matrix(PyObject *self, PyObject *args)
@@ -342,6 +342,235 @@ matrix(PyObject *self, PyObject *args)
   gsl_integration_qaws_table_free(table);
   Py_XDECREF (detectfn_coef);
   Py_DECREF(matrix);
+
+  return NULL;
+}
+
+static PyObject *
+basisfn(PyObject *self, PyObject *args)
+/* Calculates a single (k, l) PBASEX basis function and returns it as
+   a two dimensional Numpy array Python object. */
+{
+  int Rbins, Thetabins, k, l, R;
+  int wkspsize; /* Suggest: wkspsize = 100000. */
+  int midTheta;
+  double sigma, epsabs, epsrel; /* Suggest epsabs = 0.0, epsrel = 1.0e-7 */   
+  double dTheta, rk, upper_bound;
+  unsigned short int ThetabinsOdd;
+  npy_intp dims[2];
+  double *matrix = NULL;
+  gsl_integration_workspace *wksp;
+  gsl_function fn;
+  int_params params;
+  gsl_integration_qaws_table *table;
+  PyObject *numpy_matrix;
+  char *errstring;
+
+  if (!PyArg_ParseTuple(args, "iiiiddddi", 
+			&k, &l, &Rbins, &Thetabins, &sigma, &rk, &epsabs, &epsrel, &wkspsize))
+    {
+      PyErr_SetString (PyExc_TypeError, "Bad argument to matrix");
+      return NULL;
+    }
+
+  /* Release GIL, as we're not accessing any Python objects for the main calculation. */
+  Py_BEGIN_ALLOW_THREADS;
+
+  matrix = malloc(Rbins * Thetabins * sizeof (double));
+  if (matrix == NULL)
+    return PyErr_NoMemory(); 
+  
+  /* Turn off gsl error handler - we'll check return codes. */
+  gsl_set_error_handler_off ();
+
+  wksp = gsl_integration_workspace_alloc(wkspsize);
+  if (!wksp)
+    {
+      return PyErr_NoMemory();
+    }
+
+  table = gsl_integration_qaws_table_alloc(-0.5, 0.0, 0.0, 0.0);
+  if (!table)
+    {
+      gsl_integration_workspace_free(wksp);
+      return PyErr_NoMemory();
+    }
+
+  fn.function = &integrand;
+  fn.params = &params;
+
+  params.two_sigma2 = 2.0 * sigma * sigma;
+  params.rk = rk;
+  params.l = l;
+
+  /* We create a matrix for Theta = -Pi..Pi inclusive of both endpoints, despite
+     the redundancy of the last (or first) endpoint. We use the symmetry of the
+     Legendre polynomials P_L(cos(theta))=P_L(cos(-theta)) to calculate the
+     points in the range 0..Pi from those in the range -Pi..0. 
+
+     If Thetabins is an odd number, there will be a point at theta = 0, for
+     which we don't need to use the symmetry condition. If Thetabins is an even
+     number, then there won't be a point at Theta = 0. midTheta defines the last
+     point for which we need to do the calculation in either case.
+  */
+
+  dTheta = 2.0 * M_PI / (Thetabins - 1);
+  midTheta = Thetabins / 2; /* Intentionally round down. */
+  
+  if (GSL_IS_EVEN(Thetabins))
+    {
+      midTheta--;
+      ThetabinsOdd = 0;
+    }
+  else
+    {
+      ThetabinsOdd = 1;
+    }
+
+  upper_bound = rk + __UPPER_BOUND_FACTOR * sigma;
+  
+  for (R = 0; R < Rbins; R++)
+    {
+      int j;
+      params.R = (double) R;
+
+      for (j = 0; j <= midTheta; j++)
+	{
+	  int status;
+	  double result, abserr;
+	  double Theta = -M_PI + j * dTheta;
+
+	  params.RcosTheta = R * cos (Theta);
+	  
+	  if (upper_bound > R)
+	    {
+	      status = gsl_integration_qaws (&fn, (double) R, upper_bound, table,
+					     epsabs, epsrel, wkspsize,
+					     wksp, &result, &abserr);
+	    }
+	  else 
+	    {
+	      status = GSL_SUCCESS;
+	      result = 0.0;
+	      abserr = 0.0;
+	    }
+	  
+	  switch (status)
+	    {
+	      int ret;
+
+	    case GSL_SUCCESS:
+	      matrix[R * Rbins + j] = result;
+
+	      /* Symmetry of Legendre polynomials is such that
+		 P_L(cos(Theta))=P_L(cos(-Theta)), so we can exploit
+		 that here unless Theta = 0 (which only occurs if
+		 Thetabins is odd), in which case it's not needed.
+	      */
+	      if (!(ThetabinsOdd && j == midTheta))
+		matrix[R * Rbins + Thetabins - j - 1] = result;
+
+	      break;
+	      
+	    case GSL_EMAXITER:
+	      ret = asprintf(&errstring,
+			     "Failed to integrate: max number of subdivisions exceeded.\nk: %d l: %d R: %d Theta: %f\n", 
+			     k, l, R, Theta);
+	      if (ret < 0)
+		errstring = NULL;
+	      
+	      goto integration_fail;
+	      
+	    case GSL_EROUND:
+	      ret = asprintf(&errstring,
+			     "Failed to integrate: round-off error.\nk: %d l: %d R: %d Theta: %f\n", 
+			     k, l, R, Theta);
+	      if (ret < 0)
+		errstring = NULL;
+	      
+	      goto integration_fail;
+	      
+	    case GSL_ESING:
+	      ret = asprintf(&errstring,
+			     "Failed to integrate: singularity.\nk: %d l: %d R: %d Theta: %f\n", 
+			     k, l, R, Theta);
+	      if (ret < 0)
+		errstring = NULL;
+
+	      goto integration_fail;
+	      
+	    case GSL_EDIVERGE:
+	      ret = asprintf(&errstring,
+			     "Failed to integrate: divergent.\nk: %d l: %d R: %d Theta: %f\n", 
+			     k, l, R, Theta);
+	      if (ret < 0)
+		errstring = NULL;
+
+	      goto integration_fail;
+	      
+	    default:
+	      ret = asprintf(&errstring,
+			     "Failed to integrate: unknown error. status: %d.\nk: %d l: %d R: %d Theta: %f\n", 
+			     status, k, l, R, Theta);
+	      if (ret < 0)
+		errstring = NULL;
+
+	      goto integration_fail;
+	    }	
+	}
+    }
+
+  gsl_integration_workspace_free(wksp);
+  gsl_integration_qaws_table_free(table);
+
+  /* Regain GIL as we'll now access some Python objects. */
+  Py_END_ALLOW_THREADS;
+
+
+  /* Make a numpy array from matrix. */
+  dims[0] = (npy_intp) Rbins;
+  dims[1] = (npy_intp) Thetabins;
+
+  numpy_matrix = PyArray_SimpleNewFromData(2, dims, NPY_DOUBLE, (void *) matrix);
+
+  if (!numpy_matrix)
+    {
+      /* PyArray_SimpleNewFromData should already raise an exception in
+	 this event, so we just need to return NULL. */
+      free (matrix);
+      Py_XDECREF(numpy_matrix); /* Belt & braces */
+      return NULL;
+    }
+
+  /* Ensure the correct memory deallocator is called when Python
+     destroys numpy_matrix. In principle, it should be sufficient to
+     just set the OWNDATA flag on numpy_matrix, but this makes the
+     assumption that numpy uses malloc and free for memory management,
+     which it may not in the future. Note also that we are only able
+     to use malloc and free because these are guaranteed to give
+     aligned memory for standard types (which double is). For more
+     complicated objects we'd need to ensure we have aligned alloc and
+     free. See: 
+     http://blog.enthought.com/python/numpy/simplified-creation-of-numpy-arrays-from-pre-allocated-memory/
+ */
+  PyArray_SetBaseObject((PyArrayObject *) numpy_matrix, PyCObject_FromVoidPtr(matrix, free));
+
+  return numpy_matrix;
+
+ integration_fail:  
+  gsl_integration_workspace_free(wksp);
+  gsl_integration_qaws_table_free(table);
+  free (matrix);
+
+  if (errstring != NULL)
+    {
+      PyErr_SetString (IntegrationError, errstring);
+      free (errstring);
+    }
+  else
+    {
+      PyErr_SetString (IntegrationError, "Couldn't allocate storage for further detail\n");
+    }
 
   return NULL;
 }
