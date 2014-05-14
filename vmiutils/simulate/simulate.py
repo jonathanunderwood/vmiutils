2ef8f3d5f86cc037d33e74a6da714ce854ddcf43
Jonathan Underwood <jonathan.underwood@gmail.com>
2014-05-14 18:17:33 +0100

    Work on simulate.py

--- vmiutils/simulate/simulate.py
+++ vmiutils/simulate/simulate.py
@@ -3,21 +3,6 @@ import vmiutils.ChoNa as chona
 import numpy
 import numpy.polynomial.legendre as legendre
 
-def make_distribution(func, bins=128):
-    """Creates a grided representation of function 'func' which is bins
-    large in each dimension.
-    """
-    d = numpy.fromfunction(func, (dim, dim))
-
-    return d
-
-def make_image(dist):
-    """Given a gridded representation of a function, produce a
-    corresponding VMI simulated image using the forward Abel transform
-    via the Cho and Na area matrix method.
-    """
-    pass
-
 class NewtonSphere(Object):
     """Class to describe a single newton sphere definition for a particle
     group. The kinetic energy is assumed to be described by a Gaussian
@@ -30,15 +15,68 @@ class NewtonSphere(Object):
         self.beta = numpy.asarray(beta)
         self.lmax = self.beta.shape
 
-    def calc_dist(i, j):
-        """Returns probability at position (x, y).
+    def _calc_dist(i, j):
+        """Returns probability at position (i, j).
         """
         x = float (i)
         y = float (j)
         r = math.sqrt (x * x + y * y)
-        theta = math.arccos (y / r)
+        cos_theta = y / r
         a = (r - self.r0) / self.sigma
         rad = math.exp (a * a)
-        
+        ang = legendre.legval(cos_theta, beta)
 
+        return rad * ang
         
+    def cartesian_distribution(self, bins):
+        """Returns a cartesian representation of this Newton sphere (i.e. a
+        section through it). In other words, this is the modelled
+        distribution, rather than the corresponding VMI image.
+        """
+        d = numpy.from_function(_cart_dist, (bins, bins))
+        return vmi.CartesianImage(image=d)
+
+    def vmi_image(self, bins):
+        """Returns a simulated VMI image for this distribution. 
+        """
+        d = numpy.from_function(_cart_dist, (bins, bins))
+        dist = vmi.CartesianImage(image=d)
+        vmi = vmi.CartesianImage(image='empty', xbins=bins, ybins=bins)
+        
+        dim = max(dist.get_quadrant(i).shape[0] for i in xrange(4))
+
+        s = chona.area_matrix(dim)
+
+        for i in xrange(4):
+            quadrant = dist.get_quadrant(i)
+            dim = quadrant.shape[0]
+            vmi.set_quadrant(i, numpy.dot(s, quadrant))
+
+        return vmi
+
+
+if __name__ == "__main__":
+    import matplotlib.cm as cm
+    import matplotlib.plot as plt
+
+    ions = NewtonSphere(64.0, 10.0, [1.0, 2.0/math.sqrt(5.0)])
+
+    dist = ions.cartesian_distribution(128)
+    vmi = ions.vmi_image(128)
+
+
+    plt.figure(1)
+
+    plt.subplot(221)
+    plt.imshow(dist.image, cmap=cm.gist_heat, origin='lower')
+    
+    plt.subplot(222)
+    plt.imshow(dist.image, cmap=cm.spectral, origin='lower')
+
+    plt.subplot(223)
+    plt.imshow(vmi.image, cmap=cm.gist_heat, origin='lower')
+
+    plt.subplot(224)
+    plt.imshow(vmi.image, cmap=cm.spectral, origin='lower')
+
+    plt.show()
