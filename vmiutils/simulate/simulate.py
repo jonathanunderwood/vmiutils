42c1a7f275975411b4de66a4e303284bf8a9d81a
Jonathan Underwood <jonathan.underwood@gmail.com>
2014-05-16 23:36:14 +0100

    Add centre and resample arguments to vmi_image and cartesian_distribution in simulate.py.

--- vmiutils/simulate/simulate.py
+++ vmiutils/simulate/simulate.py
@@ -15,12 +15,20 @@ class NewtonSphere(object):
         self.sigma = sigma
         self.beta = numpy.asarray(beta)
 
-    def _cart_dist(self, x, y):
-        """Returns probabilities for mesh grid (x, y). Assumes the centre of
-        the image is at the centre of the grid.
+    def _cart_dist(self, x, y, **kwargs):
+        """Returns probabilities for mesh grid (x, y). If the kwargs dict has
+        a key named centre with a corresponding tuple (xc, yc) as the
+        value, we'll use that as the image centre, otherwise assumes
+        the centre of the image is at the centre of the grid.
         """
-        xc = x.shape[0] / 2.0
-        yc = y.shape[0] / 2.0
+        centre = kwargs.get('centre', None)
+
+        if centre is None:
+            xc = x.shape[0] / 2.0
+            yc = y.shape[0] / 2.0
+        else:
+            xc = centre[0]
+            yc = centre[1]
 
         # Since x and y are pixel indices, we need to calculate the
         # distance from the *centre* of the pixel (i.e. [x+0.5,
@@ -45,30 +53,58 @@ class NewtonSphere(object):
 
         return rad * ang
         
-    def cartesian_distribution(self, bins):
+    def cartesian_distribution(self, bins, centre=None, resample=False):
         """Returns a cartesian representation of this Newton sphere (i.e. a
         section through it). In other words, this is the modelled
         distribution, rather than the corresponding VMI image.
+
+        The optional 'centre' argument specifies where the image is
+        centred on the grid (default is the grid centre).
+
+        resample specifies whether to resample the image when setting
+        the centre so as to ensure the image centre lies on the lower
+        left corner of a pixel, such that the quadrants are
+        exact. This is done if resample is True. If resample is False
+        (the default) the image isn't resampled, and the quadrants are
+        taken about the nearest pixel to the image centre.
         """
         if (bins % 2) != 0:
             logger.error ('bins needs to be an even integer')
             raise ValueError('bins needs to be an even integer')
 
-        d = numpy.fromfunction(self._cart_dist, (bins, bins))
+        d = numpy.fromfunction(self._cart_dist, (bins, bins), centre=centre)
 
-        return vmi.CartesianImage(image=d)
+        return vmi.CartesianImage(image=d, centre=centre, resample=resample)
 
-    def vmi_image(self, bins):
+    def vmi_image(self, bins, centre=None, resample=False):
         """Returns a simulated VMI image for this distribution as a cartesian
         grid of binsxbins size. bins should be an even number.
+
+        The optional 'centre' argument specifies where the image is
+        centred on the grid (default is the grid centre).
+
+        resample specifies whether to resample the image when setting
+        the centre so as to ensure the image centre lies on the lower
+        left corner of a pixel, such that the quadrants are exact when
+        the VMI image is calculated. This is done if resample is
+        True. If resample is False (the default) the image isn't
+        resampled, and the quadrants are taken about the nearest pixel
+        to the image centre, and some differences between the
+        quadrants in the calculated image will occur.
         """
         if (bins % 2) != 0:
             logger.error ('bins needs to be an even integer')
             raise ValueError('bins needs to be an even integer')
 
-        d = numpy.fromfunction(self._cart_dist, (bins, bins))
-        dist = vmi.CartesianImage(image=d)
-        vmi_img = vmi.CartesianImage(image='empty', xbins=bins, ybins=bins)
+        d = numpy.fromfunction(self._cart_dist, (bins, bins),
+                               centre=centre)
+        dist = vmi.CartesianImage(image=d, centre=centre,
+                                  resample=resample)
+
+        vmi_img = vmi.CartesianImage(image='empty',
+                                     xbins=bins, ybins=bins,
+                                     centre=centre,
+                                     resample=resample)
         
         dim = max(dist.get_quadrant(i).shape[0] for i in xrange(4))
 
@@ -87,10 +123,10 @@ if __name__ == "__main__":
     import matplotlib.pyplot as plt
     import math
 
-    ions = NewtonSphere(40.0, 2.0, [1.0, 0.0, 2.0/math.sqrt(5.0)])
+    ions = NewtonSphere(20.0, 2.0, [1.0, 0.0, 2.0/math.sqrt(5.0)])
 
-    dist = ions.cartesian_distribution(128)
-    vmi_img = ions.vmi_image(128)
+    dist = ions.cartesian_distribution(64, centre=(25.5, 25.5), resample=True)
+    vmi_img = ions.vmi_image(64, centre=(25.5, 25.5), resample=True)
 
     plt.figure(1)
 
