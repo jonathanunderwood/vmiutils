281bdde019681e5e0c6ea61815bc1eafca3bf0ce
Jonathan Underwood <jonathan.underwood@gmail.com>
2015-06-11 19:44:58 +0100

    Move setup.py to use setuptools, and install numpy before building

--- setup.py
+++ setup.py
@@ -1,11 +1,27 @@
-from distutils.core import setup, Extension
-import numpy
+from setuptools import setup
+from setuptools import Extension
+from setuptools.command.build_ext import build_ext as _build_ext
+
+# http://stackoverflow.com/questions/19919905/how-to-bootstrap-numpy-installation-in-setup-py
+class build_ext(_build_ext):
+    def finalize_options(self):
+        _build_ext.finalize_options(self)
+        # Prevent numpy from thinking it is still in its setup process:
+        __builtins__.__NUMPY_SETUP__ = False
+        import numpy
+        self.include_dirs.append(numpy.get_include())
 
 setup(name='VMIutils',
       version='0.1',
       description='Programs for inverting VMI image',
       author='Jonathan G. Underwood',
       author_email='j.underwood@ucl.ac.uk',
+      cmdclass={'build_ext':build_ext},
+      setup_requires=['numpy',],
+      install_requires=['numpy',
+                        'scipy',
+                        'matplotlib',
+                        'futures',],
       packages=['vmiutils',
                 'vmiutils.pbasex',
                 'vmiutils.pbasex.detfn1',
@@ -14,20 +30,16 @@ setup(name='VMIutils',
             ],
       ext_modules=[Extension('vmiutils.pbasex._matrix', 
                              ['vmiutils/pbasex/_matrix.c'],
-                             libraries=['gsl', 'gslcblas', 'm'],
-                             include_dirs=[numpy.get_include()]),
+                             libraries=['gsl', 'gslcblas', 'm'],),
                    Extension('vmiutils.pbasex.detfn1._matrix_detfn1',
                              ['vmiutils/pbasex/detfn1/_matrix_detfn1.c'],
-                             libraries=['gsl', 'gslcblas', 'm'],
-                             include_dirs=[numpy.get_include()]),
+                             libraries=['gsl', 'gslcblas', 'm'],),
                    Extension('vmiutils.pbasex.detfn1._fit_detfn1',
                              ['vmiutils/pbasex/detfn1/_fit_detfn1.c'],
-                             libraries=['gsl', 'gslcblas', 'm'],
-                             include_dirs=[numpy.get_include()]),
+                             libraries=['gsl', 'gslcblas', 'm'],),
                    Extension('vmiutils.pbasex._fit',
                              ['vmiutils/pbasex/_fit.c'],
-                             libraries=['gsl', 'gslcblas', 'm'],
-                             include_dirs=[numpy.get_include()]),
+                             libraries=['gsl', 'gslcblas', 'm'],),
                              ],
       scripts=['scripts/pbmatrix',
                'scripts/pbmatrix_detfn1',
